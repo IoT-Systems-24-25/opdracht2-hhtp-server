8c8c12a1d150b444d187a033171da16fe9cea46e
RubenDeWindtVives <ruben.dewindt@student.vives.be>
2025-04-09 21:45:49 +0200

    elke led appart aanstuurbaar en voorbereiding inlezen van mijn sw knoppen

--- Web/index2.cgi
+++ Web/index2.cgi
@@ -7,9 +7,50 @@ t     <title>index 2</title>
 t </head>
 t <body>
 t     <img src="vives_logo.png" alt="1"/>
-c z   <input id="checkbox-id" type=checkbox name="button" disabled %s> 
-t <button id="button-on">LED ON</button>
-t <button id="button-off">LED OFF</button>
+c z   <input id="checkbox-id" type=checkbox name="button" disabled %s>
+t     
+t     <ul>
+t         <li>
+t             <button id="button-on-led1">LED1 ON</button>
+t             <button id="button-off-led1">LED1 OFF</button>
+c z           <input id="checkbox-led1" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led2">LED2 ON</button>
+t             <button id="button-off-led2">LED2 OFF</button>
+c z           <input id="checkbox-led2" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led3">LED3 ON</button>
+t             <button id="button-off-led3">LED3 OFF</button>
+c z           <input id="checkbox-led3" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led4">LED4 ON</button>
+t             <button id="button-off-led4">LED4 OFF</button>
+c z           <input id="checkbox-led4" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led5">LED5 ON</button>
+t             <button id="button-off-led5">LED5 OFF</button>
+c z           <input id="checkbox-led5" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led6">LED6 ON</button>
+t             <button id="button-off-led6">LED6 OFF</button>
+c z           <input id="checkbox-led6" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led7">LED7 ON</button>
+t             <button id="button-off-led7">LED7 OFF</button>
+c z           <input id="checkbox-led7" type=checkbox name="button" disabled %s>
+t         </li>
+t         <li>
+t             <button id="button-on-led8">LED8 ON</button>
+t             <button id="button-off-led8">LED8 OFF</button>
+c z           <input id="checkbox-led8" type=checkbox name="button" disabled %s>
+t         </li>
+t     </ul>
 t </body>
 t <script src ="script.js" defer></script>
 t </html>
--- Web/script.js
+++ Web/script.js
@@ -2,40 +2,261 @@ console.log("Hello from script.js");
 
 window.addEventListener(('load'), (event) => {
     let checkbox = document.getElementById("checkbox-id");
-    let buttonOn = document.getElementById("button-on");
-    let buttonOff = document.getElementById("button-off");
+    let checkboxled1 = document.getElementById("checkbox-led1");
+    let checkboxled2 = document.getElementById("checkbox-led2");
+    let checkboxled3 = document.getElementById("checkbox-led3");
+    let checkboxled4 = document.getElementById("checkbox-led4");
+    let checkboxled5 = document.getElementById("checkbox-led5");
+    let checkboxled6 = document.getElementById("checkbox-led6");
+    let checkboxled7 = document.getElementById("checkbox-led7");
+    let checkboxled8 = document.getElementById("checkbox-led8");
 
-    buttonOn.addEventListener('click', () => {
+    //buttons om led aan te roepen uit htm
+    let buttonOnled1 = document.getElementById("button-on-led1");
+    let buttonOffled1 = document.getElementById("button-off-led1");
+    let buttonOnled2 = document.getElementById("button-on-led2");
+    let buttonOffled2 = document.getElementById("button-off-led2");
+    let buttonOnled3 = document.getElementById("button-on-led3");
+    let buttonOffled3 = document.getElementById("button-off-led3");
+    let buttonOnled4 = document.getElementById("button-on-led4");
+    let buttonOffled4 = document.getElementById("button-off-led4");
+    let buttonOnled5 = document.getElementById("button-on-led5");
+    let buttonOffled5 = document.getElementById("button-off-led5");
+    let buttonOnled6 = document.getElementById("button-on-led6");
+    let buttonOffled6 = document.getElementById("button-off-led6");
+    let buttonOnled7 = document.getElementById("button-on-led7");
+    let buttonOffled7 = document.getElementById("button-off-led7");
+    let buttonOnled8 = document.getElementById("button-on-led8");
+    let buttonOffled8 = document.getElementById("button-off-led8");
+
+    //led1 aan en uit knoppen
+    buttonOnled1.addEventListener('click', () => {
         fetch("index2.cgi", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: "extled1=on"
-        }).then(response => { response.text() ;}).then(text => {console.log('the server said', text);}).catch(error => {console.error('Error:', error);});
-        });
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
 
-        buttonOff.addEventListener('click', () => {
-            fetch("index2.cgi", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: "extled1=off"
-            }).then(response => { response.text() ;}).then(text => {console.log('the server said', text);}).catch(error => {console.error('Error:', error);});
-        });
+    buttonOffled1.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled1=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 2 aan en uit knoppen
+    buttonOnled2.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled2=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled2.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled2=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 3 aan en uit knoppen
+    buttonOnled3.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled3=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled3.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled3=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 4 aan en uit knoppen
+    buttonOnled4.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled4=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled4.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled4=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 5 aan en uit knoppen
+    buttonOnled5.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled5=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled5.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled5=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 6 aan en uit knoppen
+    buttonOnled6.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled6=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled6.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled6=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 7 aan en uit knoppen
+    buttonOnled7.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled7=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled7.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled7=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    //led 8 aan en uit knoppen
+    buttonOnled8.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled8=on"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
+    buttonOffled8.addEventListener('click', () => {
+        fetch("index2.cgi", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: "extled8=off"
+        }).then(response => { response.text(); }).then(text => { console.log('the server said', text); }).catch(error => { console.error('Error:', error); });
+    });
+
 
     window.setInterval(
         (async () => {
             console.log("Hello from timer.js");
-            let response = await fetch("button.cgi");
-            let text = await response.text();
-            console.log(text);
-            if (text == "checked") {
+            let responsebutton = await fetch("button.cgi");
+            let responsesw1 = await fetch("led1-2.cgi");
+            let responsesw2 = await fetch("led3-4.cgi");
+            let responsesw3 = await fetch("led5-6.cgi");
+            let responsesw4 = await fetch("led7-8.cgi");
+            let button = await responsebutton.text();
+            let sw1 = await responsesw1.text();
+            let sw2 = await responsesw2.text();
+            let sw3 = await responsesw3.text();
+            let sw4 = await responsesw4.text();
+            console.log(button);
+            console.log(sw1);
+            console.log(sw2);
+            console.log(sw3);
+            console.log(sw4);
+            if (button == "checked") {
                 checkbox.setAttribute("checked", "");
             }
             else {
                 checkbox.removeAttribute("checked");
+            }
+
+            if (sw1 == "checked") {
+                checkboxled1.setAttribute("checked", "");
+                checkboxled2.setAttribute("checked", "");
+            }
+            else {
+                checkboxled1.removeAttribute("checked");
+                checkboxled2.removeAttribute("checked");
+            }
+
+            if (sw2 == "checked") {
+                checkboxled3.setAttribute("checked", "");
+                checkboxled4.setAttribute("checked", "");
+            }
+            else {
+                checkboxled3.removeAttribute("checked");
+                checkboxled4.removeAttribute("checked");
+            }
+
+            if (sw3 == "checked") {
+                checkboxled5.setAttribute("checked", "");
+                checkboxled6.setAttribute("checked", "");
+            }
+            else {
+                checkboxled5.removeAttribute("checked");
+                checkboxled6.removeAttribute("checked");
+            }
+
+            if (sw4 == "checked") {
+                checkboxled7.setAttribute("checked", "");
+                checkboxled8.setAttribute("checked", "");
+            }
+            else {
+                checkboxled7.removeAttribute("checked");
+                checkboxled8.removeAttribute("checked");
             }
         }), 3000);
 
